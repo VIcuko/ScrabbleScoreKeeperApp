8397ae741e8ced7be6b1ffd09ed6e45e6fdba05f
Vicuko <victorc.fernandez@gmail.com>
2017-04-08 22:25:17 +0200

    Create method to load info from dictionary file into array

--- app/src/main/java/com/example/android/scrabblescorekeeperapp/MainActivity.java
+++ app/src/main/java/com/example/android/scrabblescorekeeperapp/MainActivity.java
@@ -5,51 +5,85 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
     //  Tracks score for Team A
-    int eagleScore = 0;
+    int eagleScore = 3;
 
     //  Tracks score for Team B
-    int sharkScore = 0;
+    int sharkScore = 3;
 
     // Assigns values to letters in game
     String[] value_of_letters = {" ","eaionrtlsu","dg","bcmp","fhvwy","k","","","jx","","qz"};
 
-    BufferedReader word_list = new BufferedReader(new FileReader("assets/english_dictionary.txt"));
-    String str;
+    //
+    String[] word_dictionary;
 
-    List<String> list = new ArrayList<String>();
-    while((str = word_list.readLine()) != null){
-        list.add(str);
-    };
-
-    String[] stringArr = list.toArray(new String[0]);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(scoreTeamA);
+        displayForEagle(eagleScore);
+        displayForEagle(sharkScore);
+        loadDictionary();
     }
+
+    /**
+     * Loads all the words from the dictionary in the attached txt file into an array
+     */
+
+    public void loadDictionary(){
+//        int word_list = english_dictionary;
+//        InputStream dictionary_fil = getResources().openRawResource(R.raw.english_dictionary);
+        InputStream dictionary_file = getResources().openRawResource(
+                getResources().getIdentifier("english_dictionary",
+                        "raw", getPackageName()));
+
+        word_dictionary = readTextFile(dictionary_file);
+    };
+
+    /**
+     *
+     * @param inputStream file to be read
+     * @return
+     */
+    public String[] readTextFile(InputStream inputStream) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        byte buf[] = new byte[1024];
+        int len;
+        try {
+            while ((len = inputStream.read(buf)) != -1) {
+                outputStream.write(buf, 0, len);
+            }
+            outputStream.close();
+            inputStream.close();
+        } catch (IOException e) {
+
+        }
+        String[] final_array = outputStream.toString().split("\n");
+
+        return final_array;
+
+    };
 
     /**
      * Displays the given score for Team A.
      */
-    public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+    public void displayForEagle(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.eagle_score);
         scoreView.setText(String.valueOf(score));
     }
 
     /**
      * Displays the given score for Team A.
      */
-    public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+    public void displayForShark(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.shark_score);
         scoreView.setText(String.valueOf(score));
     }
 
@@ -57,26 +91,26 @@ public class MainActivity extends AppCompatActivity {
      * Increase the score of Team A by 3.
      */
     public void threePointsTeamA(View view) {
-        scoreTeamA += 3;
-        displayForTeamA(scoreTeamA);
+        eagleScore += 3;
+        displayForEagle(eagleScore);
     }
 
     /**
      * Increase the score of Team B by 3.
      */
     public void threePointsTeamB(View view) {
-        scoreTeamB += 3;
-        displayForTeamB(scoreTeamB);
+        sharkScore += 3;
+        displayForShark(sharkScore);
     }
 
     /**
      * Reset the result for both teams.
      */
     public void newGame(View view) {
-        scoreTeamA = 0;
-        scoreTeamB = 0;
-        displayForTeamA(scoreTeamA);
-        displayForTeamB(scoreTeamB);
+        eagleScore = 0;
+        sharkScore = 0;
+        displayForEagle(eagleScore);
+        displayForShark(sharkScore);
     }
 
 }
